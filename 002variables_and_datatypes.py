87609872c3f52929f917a99169696b9075edd189
pranit-ekatpure <pranitekatpure@gmail.com>
2021-02-17 18:49:42 +0530

    added programs

---
+++ 002variables_and_datatypes.py
@@ -0,0 +1,11 @@
+
+print("Variable in Python.")
+print("1. Strings\n2. Numbers\n3. Boolean")
+
+string_variable = "This is a string variable."
+number_variable = 50
+boolean_variable = True
+
+print("String: " + string_variable)
+print("Number: " + str(number_variable))
+print("Boolean: " + str(boolean_variable))
---
+++ 004numbers.py
@@ -0,0 +1,7 @@
+print(3)
+print(-2.3)
+print(3 + 4.5*2)
+print(10 % 3)
+num_var = 5
+print(num_var)
+print(str(num_var) + " Number with string")

a8fe3e06ddd7427aff24d875057d0bfc00a8099a
pranit-ekatpure <pranitekatpure@gmail.com>
2021-03-07 22:41:55 +0530

    added Learning_Python directory

---
+++ Learning_Python/script1.py
@@ -0,0 +1,7 @@
+# A first Python script
+import sys      # Laod a library module
+print(sys.platform)
+print(sys.executable)
+print(2**100)   # Raise 2 to a power
+x = 'Spam!'
+print(x*8)      # String repetition
